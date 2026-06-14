e6d1b0e8fc8b0de5780db1c397dedbbe0e506a87
fluid-movement <derandre3000@hotmail.com>
2026-06-14 21:22:50 +0200

    Update Disc Golf schedule with new workshops and events

--- src/lib/data/schedules/disc-golf.ts
+++ src/lib/data/schedules/disc-golf.ts
@@ -20,27 +20,54 @@ export const getDiscGolfSchedule = (): DisciplineSchedule => ({
 				description: '2. Runde des Turniers, Golfstart, ca. alle 12 Minuten ein Flight'
 			},
 			{
-				time: '16:00 - 17:00',
-				label: 'Weitwurf-Workshop',
+				time: '10:00 - 17:00',
+				label: 'Disc Golf für alle – einfach ausprobieren!',
 				type: 'workshop',
-        id: 'dg-distance',
+				description:
+					'Auf unserem Putting-Discgolf-Parcours mit sechs abwechslungsreichen Bahnen kannst du die Faszination des Disc Golfs ganz unkompliziert kennenlernen. Unterschiedliche Würfe und kleine Herausforderungen sorgen für Spaß bei Groß und Klein – ganz ohne Vorkenntnisse. Der Parcours ist am Samstag und Sonntag durchgehend geöffnet und kann jederzeit ausprobiert werden. Leihscheibe und Einweisung am Discgolf Turnierbüro.',
+				id: 'dg-for-all',
 				image: throwImage
+			},
+			{
+				time: '11:00 - 16:30',
+				label: 'DiscMatch-Battle',
+				type: 'workshop',
+				description: 'Zeig wie gut deine Putting Skills sind - im Battle Eins gegen Eins',
+				id: 'disc-match-battle',
+				image: discGolfPuttImage
+			},
+			{
+				time: '17:00 - 17:30',
+				label: 'DiscMatch-Battle Finale',
+				type: 'workshop',
+				description: 'Die besten Putter und Putterinnen treten im Final gegeneinander an',
+				id: 'disc-match-final',
+				image: discGolfPuttImage
+			},
+			{
+				time: '17:30 - 18:30',
+				label: 'Liquid & Maniac',
+				type: 'exhibition',
+				description:
+					'Das DiscMatch Finale wird musikalisch abgeschlossen mit feinstem boarisch Rap aus Regensburg'
 			}
 		],
 		Sonntag: [
 			{
 				time: '08:00 - 18:00',
 				label: '3. Runde Campustreffen',
-        type: 'tournament',
-        description: '3. Runde des Turniers, Golfstart, ca. alle 10 Minuten ein Flight'
+				type: 'tournament',
+				description: '3. Runde des Turniers, Golfstart, ca. alle 10 Minuten ein Flight'
 			},
 			{
-				time: '15:00 - 16:00',
-				label: 'Putting Clinic',
-        type: 'workshop',
-        id: 'dg-putt',
-        image: discGolfPuttImage,
-			},
+				time: '10:00 - 15:00',
+				label: 'Disc Golf für alle – einfach ausprobieren!',
+				type: 'workshop',
+				description:
+					'Auf unserem Putting-Discgolf-Parcours mit sechs abwechslungsreichen Bahnen kannst du die Faszination des Disc Golfs ganz unkompliziert kennenlernen. Unterschiedliche Würfe und kleine Herausforderungen sorgen für Spaß bei Groß und Klein – ganz ohne Vorkenntnisse. Der Parcours ist am Samstag und Sonntag durchgehend geöffnet und kann jederzeit ausprobiert werden.',
+				id: 'dg-putt',
+				image: throwImage
+			}
 		]
 	}
 });
