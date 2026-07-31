a42a475ce16c1e09dfbee8ae2e71c6eac3b3f0d8
fluid-movement <derandre3000@hotmail.com>
2026-07-31 20:25:11 +0200

    feat: update ultimate schedule

--- src/lib/data/schedules/ultimate.ts
+++ src/lib/data/schedules/ultimate.ts
@@ -21,46 +21,44 @@ export const getUltimateSchedule = (): DisciplineSchedule => ({
 				type: 'tournament'
 			},
 			{
-				time: '09:45 - 10:00',
-				label: 'Teamzuweisung',
-				description:
-					'Die angemeldeten SpielerInnen werden vom Veranstalter in Teams eingeteilt. Die Teammitglieder lernen sich kennen und definieren Spielstrategien für Offense und Defense',
-				type: 'tournament'
-			},
-			{
 				time: '10:00 - 10:25',
-				label: 'Gruppe A: 1-3 + 2-4',
-				description: 'Gruppe A: Spielfeld Süd - Team 1 vs 3 /  Spielfeld Nord - Team 2 vs 4',
+				label: 'Lech Layouts – Isar Insides + Main Markers – Wörthsee Wings',
+				description:
+					'Feld Süd: Lech Layouts – Isar Insides / Feld Nord: Main Markers – Wörthsee Wings',
 				type: 'tournament'
 			},
 			{
 				time: '10:30 - 10:55',
-				label: 'Gruppe B: 4-7 + 5-8',
-				description: 'Gruppe B: Spielfeld Süd - Team 4 vs 7 /  Spielfeld Nord - Team 5 vs 8',
+				label: 'Chiemsee Cutters – Donau Discs',
+				description: 'Feld Süd: Chiemsee Cutters – Donau Discs',
 				type: 'tournament'
 			},
 			{
 				time: '11:00 - 11:25',
-				label: 'Gruppe A: 3-2 + 4-1',
-				description: 'Gruppe A: SF Süd - Team 3 vs 2 /  Spielfeld Nord - Team 4 vs 1',
+				label: 'Lech Layouts – Chiemsee Cutters + Isar Insides – Donau Discs',
+				description:
+					'Feld Süd: Lech Layouts – Chiemsee Cutters / Feld Nord: Isar Insides – Donau Discs',
 				type: 'tournament'
 			},
 			{
 				time: '11:30 - 11:55',
-				label: 'Gruppe B: 7-6 + 8-5',
-				description: 'Gruppe B: SF Süd - Team 7 vs 6 /  Spielfeld Nord - Team 8 vs 5',
+				label: 'Isar Insides – Wörthsee Wings + Lech Layouts – Main Markers',
+				description:
+					'Feld Süd: Isar Insides – Wörthsee Wings / Feld Nord: Lech Layouts – Main Markers',
 				type: 'tournament'
 			},
 			{
 				time: '12:00 - 12:25',
-				label: 'Gruppe A: 1-2 + 3-4',
-				description: 'Gruppe A: SF Süd - Team 1 vs 2 /  Spielfeld Nord - Team 3 vs 4',
+				label: 'Chiemsee Cutters – Main Markers + Donau Discs – Wörthsee Wings',
+				description:
+					'Feld Süd: Chiemsee Cutters – Main Markers / Feld Nord: Donau Discs – Wörthsee Wings',
 				type: 'tournament'
 			},
 			{
 				time: '12:30 - 12:55',
-				label: 'Gruppe B: 5-6 + 7-8',
-				description: 'Gruppe B: SF Süd - Team 5 vs 6 /  Spielfeld Nord - Team 7 vs 8',
+				label: 'Lech Layouts – Donau Discs + Isar Insides – Chiemsee Cutters',
+				description:
+					'Feld Süd: Lech Layouts – Donau Discs / Feld Nord: Isar Insides – Chiemsee Cutters',
 				type: 'tournament'
 			},
 			{
@@ -68,33 +66,36 @@ export const getUltimateSchedule = (): DisciplineSchedule => ({
 				label: 'Workshop Ultimate Frisbee',
 				description:
 					'Parallel zu den ersten Finalspielen auf SF Süd findet auf dem SF Nord ein Mitmachangebot statt: ein Workshop für alle Interessierten an Ultimate Frisbee. Ihr braucht nur einigermaßen sportliche Kleidung und Turnschuhe.',
-        type: 'workshop',
-        id: 'ultimate-workshop',
-        image: workshopImage
+				type: 'workshop',
+				id: 'ultimate-workshop',
+				image: workshopImage
 			},
 			{
 				time: '13:00 - 13:25',
-				label: 'Spiel um Platz 7',
-				description: 'SF Süd: Finalspiel um Platz 7',
+				label: 'Isar Insides – Main Markers + Lech Layouts – Wörthsee Wings',
+				description:
+					'Feld Süd: Isar Insides – Main Markers / Feld Nord: Lech Layouts – Wörthsee Wings',
 				type: 'tournament'
 			},
 			{
 				time: '13:30 - 13:55',
-				label: 'Spiel um Platz 5',
-				description: 'SF Süd: Finalspiel um Platz 5',
+				label: 'Chiemsee Cutters – Wörthsee Wings + Donau Discs – Main Markers',
+				description:
+					'Feld Süd: Chiemsee Cutters – Wörthsee Wings / Feld Nord: Donau Discs – Main Markers',
 				type: 'tournament'
 			},
 			{
 				time: '14:00 - 14:25',
-				label: 'Spiel um Platz 3',
-				description: 'SF Süd: Finalspiel um Platz 3',
+				label: 'Spiel um Platz 5 + Spiel um Platz 3',
+				description:
+					'Feld Süd: 6. gegen 5. der Gruppenphase / Feld Nord: 4. gegen 3. der Gruppenphase',
 				type: 'tournament'
 			},
 			{
 				time: '14:30 - 14:55',
 				label: 'Finale - Spiel um Platz 1',
 				description:
-					'Der krönende Abschluss des Ultimate-Turniers: das Finale der beiden Gruppensieger findet auf Feld Süd des Kunstrasens vor hoffentlich vielen Zuschauern statt.',
+					'Der krönende Abschluss des Ultimate-Turniers: das Finale der beiden Erstplatzierten der Gruppenphase findet auf Feld Süd des Kunstrasens vor hoffentlich vielen Zuschauern statt.',
 				type: 'tournament'
 			},
 			{
